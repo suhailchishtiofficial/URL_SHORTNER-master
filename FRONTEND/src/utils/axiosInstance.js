703a83fda56f0d88ee647c8b2e360cb809dc92d8
suhailchishtiofficial <chishtisuhail9084@gmail.com>
2025-05-29 17:22:30 +0530

    Update axios instance baseURL to production endpoint

--- FRONTEND/src/utils/axiosInstance.js
+++ FRONTEND/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const axiosInstance = axios.create({
-    baseURL:"http://localhost:3000",
+    baseURL:"https://url-shortner-master-backend.onrender.com",
     timeout:10000, //10s
     withCredentials:true
 })
